f19213f5de76bb63e370b40f22732fdfab602379
Alex Parella <aaparella@ucdavis.edu>
2015-08-24 23:17:54 -0700

    Add Team creation
    Need to make the User creation, allow selecting of team that has
    already been created. Lots and lots to do.

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,17 @@
+class TeamsController < ApplicationController
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new team_params
+    @team.save
+    redirect_to root_path 
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name, :password)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825031035) do
+ActiveRecord::Schema.define(version: 20150825034633) do
 
   create_table "answereds", force: :cascade do |t|
     t.integer  "teamID"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20150825031035) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "password"
   end
 
   create_table "users", force: :cascade do |t|
