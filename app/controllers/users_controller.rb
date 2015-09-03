72a1ad6896fa7e9f37c6be697a0c55e19575567b
Alex Parella <aaparella@ucdavis.edu>
2015-09-02 22:24:41 -0700

    Add proof of concept loading form with AJAX
    Need to change the form to actually be the appropriate form for user
    creation. But this demonstrates how the whole loading the form via JS
    dynamically will work.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,30 @@
 class UsersController < ApplicationController
+  before_action :all_users, only: [:create, :index]
+
   def new
     @user = User.new
-  end
-
-  def index
-    @users = User.all
+    respond_to do |format| 
+      format.html
+      format.js
+    end
   end
 
   def create
     @user = User.create(user_params)
-    if params[:team_id][:team_id].empty?
-      @user.create_team(team_params)
-    else
-      @user.team = Team.find(params[:team_id][:team_id])
-    end
-    if @user.save
-      redirect_to users_path
-    else
-      render 'new'
+
+    respond_to do |format| 
+      format.html
+      format.js
     end
   end
 
   private 
 
-  def user_params 
-    params.require(:user).permit(:name, :email)
-  end
+    def all_users
+      @users = User.all
+    end
 
-  def team_params
-    params.require(:team).permit(:name, :password)
-  end
+    def user_params 
+      params.require(:user).permit(:name, :email)
+    end
 end
