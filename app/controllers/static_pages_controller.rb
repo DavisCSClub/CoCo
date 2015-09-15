c555bef1ad79ed80b94b040bedc8866e937a7765
Alex Parella <aaparella@ucdavis.edu>
2015-09-14 22:24:46 -0700

    Add Leader to Scoreboard page
    Get the current leading team, if there is one, and display it above the
    scoreboard.
    
    Resolves : #1

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,24 @@ class StaticPagesController < ApplicationController
 
   def Scoreboard
     @questions = Question.all
-    @teams = Team.all
-    @answered = Answered.all
+    @teams     = Team.all
+    @answered  = Answered.all
+    @leader    = current_leader 
   end
+
+  private 
+
+    # Return the Team that is leading, or nil if none is leading
+    def current_leader 
+      @teams = Team.order(questions_answered: :desc)
+      @first  = @teams.first
+      @second = @teams.second
+
+      if @first && (@second.nil? || @first.questions_answered > @second.questions_answered)
+        @first
+      else
+        nil
+      end
+    end
+
 end
