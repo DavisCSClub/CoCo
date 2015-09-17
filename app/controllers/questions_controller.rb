677a5528a45f2421ef9229cc8db0d47543b8ff31
Alex Parella <aaparella@ucdavis.edu>
2015-09-17 12:29:17 -0700

    Add tracking of correct answers
    Answers are now marked as answered for a given team when a user answers
    the question correctly. Warnings are provided when they are looking at a
    question their team has already answered, and they are told why they
    cannot answer a question (the submit button is removed) when they are
    not logged in, and are browsing through questions.
    
    Really coming together!

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,15 +5,22 @@ class QuestionsController < ApplicationController
 
   def show
     @question = Question.find(params[:id])
+    if !logged_in
+      flash.now[:warning] = "You cannot answer a question unless you are logged in!"
+    elsif !Answered.find_by(questionID: @question.id, teamID: current_user.team_id).nil?
+      flash.now[:warning] = "Your team has already answered this question!"
+    end 
   end
 
   def submit 
     @question = Question.find(params[:id])
     if @question.answer == params[:answer]
       flash[:success] = "Question answered successfuly!"
+      Answered.create(teamID: current_user.team_id, questionID: @question.id)
+      redirect_to questions_path
     else
       flash[:failure] = "Wrong answer"
+      redirect_to :back
     end
-    redirect_to :back
   end
 end 
